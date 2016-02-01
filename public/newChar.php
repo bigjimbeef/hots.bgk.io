8079a32381c1fc9cb826a43302246284fc50c38f
minikeen <m@bgk.io>
2016-02-01 17:42:04 +0100

    adding script

---
+++ public/newChar.php
@@ -0,0 +1,54 @@
+<?php
+
+if ($argc < 2) {
+	echo "Usage: php newChar.php [CHARNAME]\n";
+	exit();
+}
+
+$newChar = $argv[1];
+
+$file = fopen("characters", "r");
+$chars = [];
+
+$prevCharacter = "";
+$prevLine = 0;
+
+while(!feof($file)){
+    $line = fgets($file);
+
+    // Check if this is where the new character fits.
+    if (strcasecmp($newChar, $line) > 0) {
+        $prevLine = $line;
+        continue;
+    }
+
+    $prevCharacter = $prevLine;
+    break;
+}
+fclose($file);
+
+
+function writeTextAfterCharacter($file, $text, $prevCharacter) {
+
+    $lines = file($file);
+    $constantsF = fopen($file, "w+");
+
+    foreach($lines as $line)
+    {
+        fwrite($constantsF, $line);
+
+        if (stripos($line, $prevCharacter) !== false) {
+            fwrite($constantsF, $text . "\n");
+        }
+    }
+
+    fclose($constantsF);
+}
+
+$prevCharacter = trim($prevCharacter);
+
+// We now have the previous character, so read until we hit that character, then add our new one.
+writeTextAfterCharacter("characters", "$newChar", $prevCharacter);
+writeTextAfterCharacter("constants.php", "\t\t\"$newChar\",", $prevCharacter);
+writeTextAfterCharacter("empty.html", "\t\t\t\t\t\t<a href='/$newChar'><img data-name='$newChar' title='$newChar' src='/images/$newChar.png' /></a>", $prevCharacter);
+
